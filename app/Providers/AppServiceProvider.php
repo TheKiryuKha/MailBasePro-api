e9f8ca484308c442b91505737d326f418cda8b96
TheKiryuKha <kirillzuyeu@gmai.com>
2025-11-23 19:09:13 +0300

    configure some def

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
@@ -17,10 +21,29 @@ final class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->bootModelsDefaults();
+        $this->configureDBCommands();
+        $this->configureUrls();
+        $this->configureDates();
     }
 
     private function bootModelsDefaults(): void
     {
         Model::unguard();
+        Model::shouldBeStrict();
+    }
+
+    private function configureDBCommands(): void
+    {
+        DB::prohibitDestructiveCommands(app()->isProduction());
+    }
+
+    private function configureUrls(): void
+    {
+        URL::forceHttps(app()->isProduction());
+    }
+
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
     }
 }
